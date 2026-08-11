521e37d984c7f598e0dd2756e3c18f4cfe470939
sovereignuniversity20-art <sovereign.university20@gmail.com>
2026-08-11 18:49:24 -0500

    Conditions updated.

--- src/main/java/org/example/ConditionalExercises.java
+++ src/main/java/org/example/ConditionalExercises.java
@@ -2,24 +2,24 @@ package org.example;
 
 public class ConditionalExercises {
     public boolean lessThanFive(int x) {
-        // Replace the line below with code that returns true if x is less than 5, and false otherwise
-        // (use an if statement with an else block.)
-        return false;
+        return x < 5;
     }
 
     public String getAgeGroup(int age) {
-        // Replace the line below with code that returns "child" if age is less than 13,
-        // "teen" if age is less than 20, and "adult" otherwise
-        // (use if, else if, and else)
-        return "";
+              if (age < 13) {
+            return "child";
+        } else if (age < 20) {
+            return "teen";
+        } else {
+                  return "adult";
+              }
     }
 
     public boolean isValidPassword(String password) {
         int passwordLength = password.length();
-
-        // Replace the line below with code that returns true if password is at least 8 characters long
-        // and false otherwise
-        // (use an if statement with an else block.)
+            if (passwordLength > 7) {
+                return true;
+            } else
         return false;
     }
 }
