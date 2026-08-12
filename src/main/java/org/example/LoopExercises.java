4d617d13a9fe6943967f1e66e40ded16b202657f
sovereignuniversity20-art <sovereign.university20@gmail.com>
2026-08-12 13:49:51 -0500

    Conditions and Loops completed.

--- src/main/java/org/example/LoopExercises.java
+++ src/main/java/org/example/LoopExercises.java
@@ -2,15 +2,25 @@ package org.example;
 
 public class LoopExercises {
     public int sum(int n) {
-        // Replace the line below with code that returns the sum of the numbers from 1 to n
-        // (use a for loop)
-        return 0;
+        int num = 0;
+        for (int i = 1; i <=n; i++) {
+            num = num +i;
+        }
+        return num;
     }
 
     public int sumUntilEven(int n) {
         // Replace the line below with code that returns the sum of the numbers from 1 to n
         // but stops adding when the sum is even
         // (use a while loop with a sum variable and a counter variable)
-        return 0;
+        int num = 0;
+        int i = 1;
+        num = num +i;
+        i++;
+        while (num%2 != 0 && i <=n) {
+            num = num +i;
+            i++;
+        }
+        return num;
     }
 }
